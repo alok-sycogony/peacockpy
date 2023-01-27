c701d23f3a847be2ee042029a46c987d8d806a0a
alok.aryk <alok.aryk@gmail.com>
2023-01-27 15:19:16 +0530

    This is the version efining the def relationship with declared variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+def addition(x, y):
+    print("Performing Addition")
+    return x + y
+
+
+# You can alternatively use input() to fetch values from user.
+a = 3
+b = 2
+
+# Addition
+add = addition(a, b)
+print(add)
+print("\n")
